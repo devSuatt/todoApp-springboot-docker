0aeced8232ba868d2a41ef2e3a799e380443fb15
devsuatt <suat.turkoglu96@gmail.com>
2022-01-25 16:14:47 +0300

    project initialized

---
+++ src/test/java/com/devsuatt/todoApp/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuatt.todoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
